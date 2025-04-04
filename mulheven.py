f9998b6ac9834950e2ff6ade3845717426f9b2c8
Alan Hoffer <alanhoffer97@hotmail.com>
2025-04-04 11:43:39 -0300

    Fixes

--- mulheven.py
+++ mulheven.py
@@ -3,7 +3,7 @@ import time
 import random
 import threading
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Button, ttk
 from tkinter import messagebox
 from pystray import Icon, MenuItem, Menu
 from PIL import Image, ImageDraw
@@ -24,6 +24,82 @@ VK_RBUTTON = 0x02  # Código del botón derecho del mouse en Windows
 running_combo = False
 running_r = False  # Iniciar con la 'R' desactivada por defecto
 running_q = False 
+
+# Variables para controlar la selección de los combos
+selected_combo = None
+
+# Función para manejar el estado de los checkboxes
+def select_combo(combo):
+    global selected_combo
+    selected_combo = combo
+    # Desmarcar todos los checkboxes
+    combo1_var.set(False)
+    combo2_var.set(False)
+    combo3_var.set(False)
+    
+    # Marcar el checkbox del combo seleccionado
+    if combo == 'Combo 1':
+        combo1_var.set(True)
+    elif combo == 'Combo 2':
+        combo2_var.set(True)
+    elif combo == 'Combo 3':
+        combo3_var.set(True)
+
+# Definir los combos con teclas y tiempos
+combos = {
+    # 1st Skill Order
+    # 1. Cyclone Slash( Weapon Skill) 
+    # 2. Death Stab
+    # 4. Rageful Blow
+
+    # 2nd Skill Order
+    # 1. Cyclone Slash( Weapon Skill)
+    # 3. Twisting slash.
+    # 4. Rageful Blow
+    'Combo 1': [
+            (1, 135), 
+            (1, 63),  
+            (4, 120),  
+            (4, 39),   
+            (2, 152),  
+            (2, 159),  
+            (1, 119),  
+            (1, 103), 
+            (4, 104), 
+            (4, 72),  
+            (2, 152), 
+            (2, 295),
+
+            (1, 112),
+            (1, 95),   
+            (4, 104),  
+            (4, 79),   
+            (3, 152),  
+            (3, 184),  
+            (1, 128),  
+            (1, 95),   
+            (4, 159),  
+            (4, 39),   
+            (3, 128), 
+    ],
+    # 1. Cyclone Slash( Weapon Skill)
+    # 2. Death Stab
+    # 3. Twisting slash
+    # 4. Rageful Blow
+    'Combo 2': [
+            (1, 135), 
+            (4, 120),  
+            (2, 152),  
+            (3, 152), 
+            (1, 100), 
+            (4, 104),  
+            (2, 152),  
+            (3, 152)  
+    ],
+    'Combo 3': [
+        (1, 80), (2, 70), (3, 40)
+    ]
+}
 
 # Función para obtener el estado de una tecla o botón del mouse
 def is_key_pressed(vk_code):
@@ -46,20 +122,6 @@ def press_key(key):
     ctypes.windll.user32.keybd_event(key_code, 0, KEYEVENTF_KEYUP, 0)
     print(f"Tecla {key.upper()} liberada")
 
-# Función que ejecuta la secuencia de teclas mientras se mantiene el clic derecho presionado
-def press_combo_while_right_click(cooldown, cant_poderes):
-    global running_combo
-    print(f"Iniciando el combo con cooldown de {cooldown} ms y {cant_poderes} poderes...")  # Mensaje de inicio de combo
-    while running_combo:
-        if is_key_pressed(VK_RBUTTON):
-            print("Botón derecho presionado, comenzando combo...")  # Mensaje en consola
-            while is_key_pressed(VK_RBUTTON) and running_combo:
-                for i in range(cant_poderes):
-                    print(f"Presionando el poder {i+1}")
-                    press_key(str(1 + i))
-                    time.sleep(get_random_cooldown(cooldown, 10) / 1000)
-        time.sleep(1)  # Pequeña pausa para evitar uso excesivo de CPU
-        print("Esperando 200 ms antes de verificar el estado del botón derecho")
 
 # Función que presiona la tecla 'R' cada 60 segundos
 def auto_press_r():
@@ -80,34 +142,43 @@ def auto_press_q():
         press_key('Q')
 
 
-# Función para iniciar/detener el combo
-def toggle_combo():
+# Función para presionar teclas y gestionar los tiempos de delay
+def execute_combo(combo):
     global running_combo
+    print(f"Iniciando el combo...")  
+    for key, delay in combo:
+        if not running_combo:
+            break
+        press_key(str(key))  # Simula presionar la tecla
+        time.sleep(delay / 1000)  # Delay entre las teclas
+        press_key(str(key))  # Simula soltar la tecla
+        time.sleep(0.1)  # Delay después de presionar la tecla
+
+
+# Función que ejecuta la secuencia de teclas mientras se mantiene el clic derecho presionado
+def press_combo_while_right_click(combo):
+    global running_combo
+    print(f"Iniciando el combo con teclas {combo}...")
+    while running_combo:
+        if is_key_pressed(VK_RBUTTON):
+            print("Botón derecho presionado, comenzando combo...")
+            execute_combo(combo)
+
+
+def toggle_combo():
+    global running_combo, selected_combo
     if running_combo:
         running_combo = False
         print("Deteniendo el combo...")
         combo_button.config(text="Iniciar Combo")
     else:
-        try:
-            poderes = int(power_entry.get())
-            cooldown = int(cooldown_entry.get())
-            print(f"Configuración del combo: {poderes} poderes, cooldown de {cooldown} ms")
-            if not (0 <= poderes <= 5):
-                messagebox.showerror("Error", "La cantidad de poderes debe estar entre 0 y 5.")
-                print("Error: La cantidad de poderes no está entre 0 y 5")
-                return
-            if cooldown < 50:
-                messagebox.showerror("Error", "El cooldown debe ser de al menos 50 ms.")
-                print("Error: El cooldown es menor de 50 ms")
-                return
-        except ValueError:
-            messagebox.showerror("Error", "Ingrese valores numéricos válidos.")
-            print("Error: Valores no numéricos ingresados")
+        if not selected_combo:
+            messagebox.showerror("Error", "Seleccione un combo primero.")
             return
-        
         running_combo = True
-        print("Iniciando el hilo del combo...")
-        threading.Thread(target=press_combo_while_right_click, args=(cooldown, poderes), daemon=True).start()
+        print(f"Iniciando el hilo del {selected_combo}...")
+        selected_combo_keys = combos[selected_combo]  # Obtiene las teclas del combo seleccionado
+        threading.Thread(target=press_combo_while_right_click, args=(selected_combo_keys,), daemon=True).start()
         combo_button.config(text="Detener Combo")
 
 # Función para activar/desactivar la tecla 'R'
@@ -214,48 +285,41 @@ checkbox_frame = tk.Frame(root, bg='#DBAFA9')
 checkbox_frame.pack(pady=10, anchor='w')  # Alineamos el frame a la izquierda
 
 q_checkbox = ttk.Checkbutton(checkbox_frame, text="Auto Potion (Q)", variable=q_check_var, command=toggle_q, style="Custom.TCheckbutton")
-q_checkbox.grid(row=0, column=0, padx=10, pady=5, sticky='w')  # Alineación a la izquierda
+q_checkbox.grid(row=0, column=0, padx=10, pady=3, sticky='w')  # Alineación a la izquierda
 
 r_checkbox = ttk.Checkbutton(checkbox_frame, text="Auto Ulti 'R'", variable=r_check_var, command=toggle_r, style="Custom.TCheckbutton")
-r_checkbox.grid(row=1, column=0, padx=10, pady=5, sticky='w')  # Alineación a la izquierda
+r_checkbox.grid(row=1, column=0, padx=10, pady=3, sticky='w')  # Alineación a la izquierda
 
 
 
 # Frame para contener la etiqueta y el Entry
 powerFrame = tk.Frame(root, bg='#DBAFA9')
-powerFrame.pack(pady=5, padx=10, anchor="w")  # 'w' para alinear a la izquierda (west)
+powerFrame.pack(pady=2, padx=10, anchor="w")  # 'w' para alinear a la izquierda (west)
+
+# Variables de control para los checkboxes
+combo1_var = tk.BooleanVar()
+combo2_var = tk.BooleanVar()
+combo3_var = tk.BooleanVar()
+
+title_label2 = tk.Label(powerFrame, text="1.Cycl 2.Rage 3.Stab 4.Twist", font=("Arial", 8, "bold"), bg="#DBAFA9")  # Cambia el color del texto a rojo
+title_label2.pack(pady=2)
+
+# Crear los checkboxes
+combo1_checkbox = ttk.Checkbutton(powerFrame, text="Combo 1", variable=combo1_var, onvalue=True, offvalue=False, command=lambda: select_combo('Combo 1'), style="Custom.TCheckbutton")
+combo1_checkbox.pack(anchor="w")
 
 
-
-# Entry alineado a la izquierda
-power_entry = ttk.Entry(powerFrame, font=("Arial", 10), width=5)
-power_entry.grid(row=0, column=0)
-
-# Etiqueta alineada a la derecha
-power_label = ttk.Label(powerFrame, text="Skills (0-5):")
-power_label.grid(row=0, column=1, padx=(0, 10))  # Ajusta la distancia con padx
+combo2_checkbox = ttk.Checkbutton(powerFrame, text="Combo 2", variable=combo2_var, onvalue=True, offvalue=False, command=lambda: select_combo('Combo 2'), style="Custom.TCheckbutton")
+combo2_checkbox.pack(anchor="w")
 
 
-# Frame para contener la etiqueta y el Entry
-cooldownFrame = tk.Frame(root, bg='#DBAFA9')
-cooldownFrame.pack(pady=5, padx=10, anchor="w")  # 'w' para alinear a la izquierda (west)
+combo3_checkbox = ttk.Checkbutton(powerFrame, text="Combo 3", variable=combo3_var, onvalue=True, offvalue=False, command=lambda: select_combo('Combo 3'), style="Custom.TCheckbutton")
+combo3_checkbox.pack(anchor="w")
 
 
-
-# Entry alineado a la izquierda
-cooldown_entry = ttk.Entry(cooldownFrame, font=("Arial", 10), width=5)
-cooldown_entry.grid(row=1, column=0)
-
-# Etiqueta alineada a la derecha
-cooldown_label = ttk.Label(cooldownFrame, text="Cooldown (ms, min 50):")
-cooldown_label.grid(row=1, column=1, padx=(0, 10))  # Ajusta la distancia con padx
-
-
-
-
-# Botón para iniciar combo
-combo_button = ttk.Button(root, text="Iniciar Combo", command=toggle_combo)
-combo_button.pack(pady=10)
+# Botón para iniciar el combo
+combo_button = tk.Button(powerFrame, text="Iniciar Combo", command=toggle_combo)
+combo_button.pack(pady=4, anchor="w")
 
 # Se puede agregar más widgets de este estilo, como checkboxes, sliders, etc.
 
